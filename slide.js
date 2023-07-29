b4cd4a570775fed4ff8913c6a8c38ec44a434841
Richard Tang <rtang400@gmail.com>
2023-07-28 22:10:09 -0700

    Added a video to a slideshow

--- slide.js
+++ slide.js
@@ -1,8 +1,9 @@
 let slideIndex = [1,1];
 /* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides1", "mySlides2"]
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
