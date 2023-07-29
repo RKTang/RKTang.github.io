02a5fbfd2f8a335999003f618691f6c59221a628
Richard Tang <rtang400@gmail.com>
2023-07-29 03:44:20 -0700

    Update slide.js

--- slide.js
+++ slide.js
@@ -21,10 +21,6 @@ function showSlides(n, no) {
   if (n < 1) { slideIndex[no] = x.length; }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
-    if (x[i].querySelector("video")) {
-      const videoElement = x[i].querySelector("video");
-      videoElement.pause(); // Pause all videos
-    }
   }
   if (x[slideIndex[no] - 1]) {
     x[slideIndex[no] - 1].style.display = "block";
