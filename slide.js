1071cae904a1ff27c42dc66879a35e4413bc6385
Richard Tang <rtang400@gmail.com>
2023-07-29 23:48:44 -0700

    Updated the slideshow code

--- slide.js
+++ slide.js
@@ -1,37 +1,37 @@
-let slideIndex = [1, 1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"];
-
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
+const slideIndex = [1, 1];
+const slideIds = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"];
 
 function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+  showSlides(slideIndex[no] + n, no);
 }
 
 function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (x.length === 0) {
-    console.error("No elements found with class name: " + slideId[no]);
+  const slides = document.querySelectorAll(`.${slideIds[no]}`);
+  if (!slides.length) {
+    console.error(`No elements found with class name: ${slideIds[no]}`);
     return;
   }
-  if (n > x.length) { slideIndex[no] = 1; }
-  if (n < 1) { slideIndex[no] = x.length; }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+
+  if (n > slides.length) {
+    slideIndex[no] = 1;
+  } else if (n < 1) {
+    slideIndex[no] = slides.length;
+  } else {
+    slideIndex[no] = n;
   }
-  if (x[slideIndex[no] - 1]) {
-    x[slideIndex[no] - 1].style.display = "block";
-    if (x[slideIndex[no] - 1].querySelector('video')) {
-      const videoElement = x[slideIndex[no] - 1].querySelector('video');
-      videoElement.style.display = "block";
-      videoElement.play(); // Auto-play the video
+
+  slides.forEach((slide, index) => {
+    slide.style.display = index === slideIndex[no] - 1 ? "block" : "none";
+
+    const videoElement = slide.querySelector('video');
+    if (videoElement) {
+      if (index === slideIndex[no] - 1) {
+        videoElement.style.display = "block";
+        videoElement.play(); // Auto-play the video
+      } else {
+        videoElement.style.display = "none";
+        videoElement.pause();
+      }
     }
-  }
+  });
 }
-
-
-
-
