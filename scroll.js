78fe04d9e2ff5ed814845d9dfa1604adab487c1c
Richard Tang <rtang400@gmail.com>
2023-07-31 00:42:39 -0700

    Updated scroll.js

--- scroll.js
+++ scroll.js
@@ -1,19 +1,19 @@
-// Get the button:
-let mybutton = document.getElementById("myBtn");
+    // Show/hide the "scroll to top" button based on the scroll position
+    window.onscroll = function() {
+        showScrollButton();
+    };
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+    function showScrollButton() {
+        var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    }
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+    // Scroll to top functionality
+    function scrollToTop() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    }
